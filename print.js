a96aab668f8170cf4bd551fe61892d628331f8b5
Core Wave <1063912949@qq.com>
2026-04-11 16:50:28 +0800

    Update print.js

--- print.js
+++ print.js
@@ -1,12 +1,5 @@
-// JavaScript source code
-
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".start-btn");
-  btn.addEventListener("click", () => {
-    const paraEl = document.createElement("p");
-    const bodyEl = document.querySelector("body");
-
-    bodyEl.appendChild(paraEl);
-    paraEl.textContent = "Ciallo～(∠・ω< )⌒⭐";
-  });
-});
+body {
+  background-image: url(https://images.unsplash.com/photo-1775418448845-02fcd8c7152c?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  font-family: sans-serif;
+  padding: 20px;
+}
